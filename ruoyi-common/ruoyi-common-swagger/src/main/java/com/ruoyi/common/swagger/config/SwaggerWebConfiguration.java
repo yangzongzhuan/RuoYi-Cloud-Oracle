c0677aab8bf87ba40c78952cfb16d0541fe20a40
RuoYi <yzz_ivy@163.com>
2023-03-29 10:51:21 +0800

    修复注释 @EnableCustomSwagger2 后，项目启动失败

--- ruoyi-common/ruoyi-common-swagger/src/main/java/com/ruoyi/common/swagger/config/SwaggerWebConfiguration.java
+++ ruoyi-common/ruoyi-common-swagger/src/main/java/com/ruoyi/common/swagger/config/SwaggerWebConfiguration.java
@@ -1,6 +1,5 @@
 package com.ruoyi.common.swagger.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 
  * @author ruoyi
  */
-@Configuration
 public class SwaggerWebConfiguration implements WebMvcConfigurer
 {
     @Override
