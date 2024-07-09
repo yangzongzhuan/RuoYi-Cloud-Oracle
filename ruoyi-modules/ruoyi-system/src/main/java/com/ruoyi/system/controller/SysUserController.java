2c69d1c5c1332cac8808cb5774c785d72669710f
RuoYi <yzz_ivy@163.com>
2024-07-09 12:23:44 +0800

    记录用户登录IP地址和登录时间

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
@@ -150,6 +150,16 @@ public class SysUserController extends BaseController
     }
 
     /**
+     *记录用户登录IP地址和登录时间
+     */
+    @InnerAuth
+    @PutMapping("/recordlogin")
+    public R<Boolean> recordlogin(@RequestBody SysUser sysUser)
+    {
+        return R.ok(userService.updateUserProfile(sysUser));
+    }
+
+    /**
      * 获取用户信息
      * 
      * @return 用户信息
